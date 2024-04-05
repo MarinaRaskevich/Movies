e021a6a687065e2e5d05769a5964f0bde8f78768
MarinaRaskevich <marina.raskevich19@gmail.com>
2024-04-05 16:33:28 +0200

    Last update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,38 +1,89 @@
+// Sélecteurs
 const row = document.querySelector(".row");
+const title = document.querySelector("#title");
 
+// Fetch
 fetch("/public/assets/json/movies.json")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     getData(data);
-  })
-  .catch((error) => console.error("Error loading JSON file", error));
+  });
 
+// Function principale
 const getData = (data) => {
   const movies = data.results;
+  title.innerText = `Sélection de ${movies.length} films populaires`;
 
   movies.forEach((movie) => {
-    // col
-    const div = document.createElement("div");
-    div.classList.add("col-2");
+    // Vérifier s'il y a une description pour le film
+    const overview =
+      movie.overview == "" ? "Pas de déscription" : movie.overview;
 
-    // img
-    const img = document.createElement("img");
-    img.setAttribute(
-      "src",
-      `https://image.tmdb.org/t/p/original${movie.poster_path}`
-    );
-    img.setAttribute("alt", `${movie.title}`);
-    img.classList.add("w-100");
+    // Couleur de rate
+    const rateColor =
+      movie.vote_average >= 7 ? "text-success" : "text-secondary";
 
-    // title
-    const title = document.createElement("h2");
-    title.classList.add("fs-5", "text-center", "fw-bold");
-    title.textContent = `${movie.title}`;
+    // Date française
+    let date = Date.parse(movie.release_date);
+    date = new Date(date);
+    date = date.toLocaleString("fr", { dateStyle: "long" });
 
-    div.appendChild(img);
-    div.appendChild(title);
-    row.appendChild(div);
+    row.innerHTML += `
+    <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+      <div class="card border-0 bg-transparent">
+        <img src="https://image.tmdb.org/t/p/original${
+          movie.poster_path
+        }" alt="${movie.title}" class="w-100 poster">
+        <div class="card-body">
+          <h5 class="card-title text-white">${movie.title}</h5>
+          <div class="d-flex justify-content-between">
+            <p class="card-text ${rateColor}"><i class="bi bi-star-fill ${rateColor}"></i> ${parseFloat(
+      movie.vote_average.toFixed(1)
+    )}</p>
+            <a type="button" data-bs-toggle="modal" data-bs-target="#modal${
+              movie.id
+            }" class="text-secondary text-decoration-none">
+              Plus d'info
+            </a>
+          </div>
+          
+          <div class="modal fade" id="modal${
+            movie.id
+          }" tabindex="-1" aria-labelledby="#modalLabel${
+      movie.id
+    }" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+              <div class="modal-content bg-black">
+                <div class="modal-header border-0">
+                  <div>
+                    <h3 class="modal-title fs-5 fw-bold text-white" id="modalLabel${
+                      movie.id
+                    }">${movie.title}</h3>
+                    <p class="m-0 text-secondary">${movie.original_title}</p>
+                  </div>
+                </div>
+                <div class="modal-body row py-4 text-white">
+                  <div class="col-4">
+                    <img src="https://image.tmdb.org/t/p/original${
+                      movie.poster_path
+                    }" alt="${movie.title}" class="w-100 poster">
+                  </div>
+                  <div class="col-8 d-flex flex-column justify-content-between">
+                    <h4>Déscription</h4>
+                    <p class="pe-4">${overview}</p>
+                    <p class="text-secondary m-0">Date de sortie: ${date}</p>
+                  </div>
+                </div>
+                <div class="modal-footer border-0">
+                  <button type="button" class="btn btn-light" data-bs-dismiss="modal">Fermer</button>
+                </div>
+              </div>
+            </div>
+          </div>        
+        </div>
+      </div>
+    </div> `;
   });
 };
--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,8 +1,5 @@
-// Sélecteurs
 const row = document.querySelector(".row");
-const title = document.querySelector("#title");
 
-// Fetch
 fetch("/public/assets/json/movies.json")
   .then((response) => {
     return response.json();
@@ -12,42 +9,30 @@ fetch("/public/assets/json/movies.json")
   })
   .catch((error) => console.error("Error loading JSON file", error));
 
-// Function principale
 const getData = (data) => {
   const movies = data.results;
-  title.innerText = `Sélection de ${movies.length} films intéressants`;
 
   movies.forEach((movie) => {
-    // Vérifier s'il y a une description pour le film
-    const overview =
-      movie.overview == "" ? "Pas de déscription" : movie.overview;
-    row.innerHTML += `
-    <div class="col-4">
-        <div class="row films">
-            <div class="col-4">
-                <img class="w-100" src="https://image.tmdb.org/t/p/original${
-                  movie.poster_path
-                }" alt="${movie.title}">
-            </div>
-            <div class="col-8 d-flex flex-column h-100">
-                <h2 class="fs-5 fw-bold mb-3">${movie.title}</h2>
-                <p class="fw-bold fs-6 mt-auto"><i class="bi bi-star-fill"></i> ${parseFloat(
-                  movie.vote_average.toFixed(1)
-                )}</p>
-                <a
-                  type="button"
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                >
-                  Savoir plus
-                </a>
-            </div>
-        </div>
-    </div>
-    `;
+    // col
+    const div = document.createElement("div");
+    div.classList.add("col-2");
+
+    // img
+    const img = document.createElement("img");
+    img.setAttribute(
+      "src",
+      `https://image.tmdb.org/t/p/original${movie.poster_path}`
+    );
+    img.setAttribute("alt", `${movie.title}`);
+    img.classList.add("w-100");
+
+    // title
+    const title = document.createElement("h2");
+    title.classList.add("fs-5", "text-center", "fw-bold");
+    title.textContent = `${movie.title}`;
+
+    div.appendChild(img);
+    div.appendChild(title);
+    row.appendChild(div);
   });
 };
-
-// <div class="h-50 description">
-// <p>${overview}</p>
-// </div>
